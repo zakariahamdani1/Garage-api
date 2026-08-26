132775c0af6a365f87f4286710790b26e44515b0
zakaria hamdani <hamdanizakaria86@gmai.com>
2026-08-26 07:26:13 +0200

    Configure allowed host for production

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fcvw(j*9e(i!0q*d71@p!f_1+_qdinw@jrg#f4_2-v&mq%c801'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["garage-api-iein.onrender.com"]
 
 
 # Application definition
